358827b3171fc31ce1c73b0bd18c6cbf3ec5d00a
Guilherme Vitório <gui.vitoriot@gmail.com>
2025-04-07 09:49:48 -0300

    Enviando exercicio01

---
+++ exercicio01.py
@@ -0,0 +1,31 @@
+n=int(input("Digite um número de 1-12: "))
+if n>=1 or n>12:
+    print("DIGITA CERTO MIZERAAA!!!!")
+    if n==1:
+        print("Seu mês foi Janeiro!")
+    elif n==2:
+        print("Seu mês foi Fevereiro!")
+    elif n==3:
+        print("Seu mês foi Março!")
+    elif n==4:
+        print("Seu mês foi Abril!")
+    elif n==5:
+        print("Seu mês foi Maio!")
+    elif n==6:
+        print("Seu mês foi Junho!")
+    elif n==7:
+        print("Seu mês foi Julho!")
+    elif n==8:
+        print("Seu mês foi Agosto!")
+    elif n==9:
+        print("Seu mês foi Setembro!")
+    elif n==10:
+        print("Seu mês foi Outubro!")
+    elif n==11:
+        print("Seu mês foi Novembro!")
+    elif n==12:
+        print("Seu mês foi Dezembro!")
+
+
+
+
