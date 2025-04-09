9e8d23e3ee122fa2e244fd12caee0aa86ed7c87d
Guilherme Vitório <gui.vitoriot@gmail.com>
2025-04-08 22:49:02 -0300

    Iniciando Atv aula04

---
+++ exercicio02.py
@@ -0,0 +1,4 @@
+nome= input("Digite seu nome: ")
+for i in range (0,3,1):
+    print(nome)
+
---
+++ exercicio05.py
@@ -0,0 +1,5 @@
+
+num= int(input("Digite um número: "))
+for i in range(1,11,1):
+    res = i * num
+    print(i,"x",num,"=",res)
